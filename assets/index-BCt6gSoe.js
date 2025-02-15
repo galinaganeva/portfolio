8aebf0df733358c7e208bf791d1e2861fc7a3eee
Stefan Gavrailov <stefan.gavrailov01@gmail.com>
2025-02-16 00:54:55 +0200

    Updates

---
+++ assets/index-BCt6gSoe.js
@@ -0,0 +1,56 @@
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();document.addEventListener("DOMContentLoaded",()=>{document.querySelector("#app").innerHTML=`
+    <header>
+      <div class="logo-container">
+        <h1 class="logo">GALINA GANEVA</h1>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">RESUME</a></li>
+          <img src="portfolio-new/arrow.svg" width="32" height="32" />
+        </ul>
+      </nav>
+    </header>
+    <main id="main">
+    </main>
+    <footer>
+      <p>2025</p>
+      <p>Galina Ganeva</p>
+    </footer>
+  `});const a=`
+  <section style="position: relative; width: 100%;">
+    <div class="hero-container">
+      <h1 class="hero-title left">Less Noise,</h1>
+      <h1 class="hero-title middle">More Clarity,</h1>
+      <h1 class="hero-title right">Bold Impact.</h1>
+    </div>
+    <div class="spline-container">
+      <spline-viewer
+        url="https://prod.spline.design/K-5M7t3eeTR7hjzy/scene.splinecode"
+      ></spline-viewer>
+    </div>
+  </section>
+`,c=`
+  <section class="about-section">
+    <h1 class="about-text">
+      I am a passionate designer in the iGaming industry, crafting
+      visually engaging experiences that captivate players and enhance
+      brand identity.
+    </h1>
+  </section>
+`,l=[{id:1,number:"01",name:"Grandz Bet",tools:["Website","Figma","2023"]},{id:2,number:"02",name:"Space Fortuna",tools:["Website","Figma","2023"]},{id:3,number:"03",name:"Grandz Race",tools:["Website","Figma","2023"]},{id:4,number:"04",name:"Tortuga",tools:["Landing Page","Webflow","2023"]},{id:5,number:"05",name:"Casino En Ligne Nouveau",tools:["Website","Figma","2023"]}],d=()=>`
+    <section class="projects">
+      ${l.map(o=>`
+        <div class="project" key="${o.id}">
+          <h3 class="project-number">${o.number}</h3>
+          <div class="project-info">
+            <h2 class="project-name">${o.name}</h2>
+            <div class="project-tools">
+              ${o.tools.map(n=>`
+                <h4 class="project-item">${n}</h4>
+              `).join("")}
+            </div>
+          </div>
+        </div>
+      `).join("")}
+    </section>
+  `;document.addEventListener("DOMContentLoaded",()=>{var i;const o=document.querySelector("#main");o&&(o.innerHTML+=a,o.innerHTML+=c,o.innerHTML+=d());const n=document.querySelector("spline-viewer");if(n){const e=(i=n.shadowRoot)==null?void 0:i.querySelector("#logo");e&&e.remove()}document.querySelectorAll(".project").forEach((e,t)=>{e.addEventListener("click",()=>{window.location.href=`/portfolio-new/project${t+1}.html`})})});
