733185aab8e61cbacc837d34010004a0e505e38f
Stefan Gavrailov <stefan.gavrailov01@gmail.com>
2025-02-16 01:00:36 +0200

    Add vite-plugin-static-copy for static file handling; update package.json and package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,21 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 export default defineConfig({
   base: "/portfolio-new",
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "project-template.html",
+          dest: ".",
+        },
+        {
+          src: "project1.html",
+          dest: ".",
+        },
+        // Add other project HTML files here
+      ],
+    }),
+  ],
 });
